da651d1173332013d29f7ee6d286f88b95727224
dejanV91 <dejanvukoicic91@gmail.com>
2024-01-05 11:56:11 +0100

    finished table

--- src/app/home/list/list.component.ts
+++ src/app/home/list/list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges} from '@angular/core';
+import { Bus } from 'src/app/models/bus';
+import { BusStationWithBuses } from 'src/app/models/busStationWithBuses';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnChanges {
+
+  buses:Bus[]=[];
+
+  @Input() busStationWithBuses:BusStationWithBuses = new BusStationWithBuses();
+
+  ngOnChanges(): void {
+    if (this.busStationWithBuses.vehicles) {
+      this.buses = this.busStationWithBuses.vehicles.sort((a:Bus,b:Bus)=>
+        a.stationsBetween - b.stationsBetween
+      ).slice(0,15);
+    }
+  }
 
 }
