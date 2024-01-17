b7b6ae3ee8a09117b8d7712d88bdb691daf7e40f
dejanV91 <dejanvukoicic91@gmail.com>
2024-01-17 09:13:09 +0100

    change path to icons

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -19,17 +19,19 @@ import { BusplusService } from 'src/app/services/busplus.service';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
-export class MapComponent implements AfterViewInit,OnChanges {
-  
+export class MapComponent implements AfterViewInit, OnChanges {
   @ViewChild('map') private mapContainer!: ElementRef<HTMLElement>;
-  map:any;
+  map: any;
 
   constructor() {}
 
-  @Input() busStationWithBuses:BusStationWithBuses = new BusStationWithBuses()
+  @Input() busStationWithBuses: BusStationWithBuses = new BusStationWithBuses();
 
   ngAfterViewInit(): void {
-    this.map = L.map(this.mapContainer.nativeElement).setView([44.787197, 20.457273],11);
+    this.map = L.map(this.mapContainer.nativeElement).setView(
+      [44.787197, 20.457273],
+      11
+    );
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
@@ -38,60 +40,53 @@ export class MapComponent implements AfterViewInit,OnChanges {
     }).addTo(this.map);
   }
 
-  ngOnChanges(): void {    
-   let currentId = '';
+  ngOnChanges(): void {
+    let currentId = '';
     if (currentId != this.busStationWithBuses.id) {
-      this.map.eachLayer((layer:any) => {
+      this.map.eachLayer((layer: any) => {
         if (layer instanceof L.Marker) {
           layer.remove();
         }
-      })
+      });
     }
     if (this.busStationWithBuses.coords.length) {
       //find station
-      this.findStation(this.busStationWithBuses.coords[0],this.busStationWithBuses.coords[1]);
+      this.findStation(
+        this.busStationWithBuses.coords[0],
+        this.busStationWithBuses.coords[1]
+      );
 
-     //find buses
-     this.busStationWithBuses.vehicles.forEach((bus:Bus)=>{
-      this.findBus(bus);
-     })
+      //find buses
+      this.busStationWithBuses.vehicles.forEach((bus: Bus) => {
+        this.findBus(bus);
+      });
     }
 
     currentId = this.busStationWithBuses.id;
   }
 
-  findStation(x:string,y:string){
+  findStation(x: string, y: string) {
     let stationIcon = markerColor('yellow');
-    L.marker([
-      Number(x),
-      Number(y),
-    ],{icon:stationIcon}).addTo(this.map);
+    L.marker([Number(x), Number(y)], { icon: stationIcon }).addTo(this.map);
 
-    this.map.flyTo([
-      Number(x),
-      Number(y),
-    ], 14);
+    this.map.flyTo([Number(x), Number(y)], 14);
   }
 
-  findBus(bus:Bus){
+  findBus(bus: Bus) {
     let stationIcon = markerColor('blue');
-    L.marker([
-      Number(bus.coords[0]),
-      Number(bus.coords[1]),
-    ],{icon:stationIcon})
-    .addTo(this.map)
-    .bindPopup(`${bus.lineNumber}`)
-    .bindTooltip(`${bus.lineNumber}`).openTooltip();
+    L.marker([Number(bus.coords[0]), Number(bus.coords[1])], {
+      icon: stationIcon,
+    })
+      .addTo(this.map)
+      .bindPopup(`${bus.lineNumber}`)
+      .bindTooltip(`${bus.lineNumber}`)
+      .openTooltip();
   }
-
-
-
-  
 }
 
-function markerColor(color:string){
+function markerColor(color: string) {
   return L.icon({
-    iconUrl:`../../../assets/images/icon${color}.png`, 
+    iconUrl: `assets/images/icon${color}.png`,
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -30],
